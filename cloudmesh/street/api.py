1396501b017fd0055e55f571065ef5c454c84bed
Gregor von Laszewski <laszewski@gmail.com>
2017-04-30 07:42:47 -0400

    new:usr: added framework to use cmd5

---
+++ cloudmesh/street/api.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+#import cv2
+
+class Street(object):
+
+    def __init__(self):
+        # not your location will not work easily / is not writeable
+        self.classifier = '/street-signal/classifier/stopsign_classifier.xml'
+        
+    def hello(self, msg):
+        print ("Hello Street", msg)
+
+    def detect(self, image):
+        '''
+        stop_cascade = cv2.CascadeClassifier(self.classifier)
+        test = cv2.imread(image)
+        gray = cv2.cvtColor(test,cv2.COLOR_BGR2GRAY)
+        stops = stop_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2)
+        for (x,y,w,h) in stops:
+            cv2.rectangle(test,(x,y),(x+w,y+h),(255,0,0),2)
+        return test
+        '''
+        return image  # remove once you fix the above
+
+        
--- cloudmesh/street/command/street.py
+++ cloudmesh/street/command/street.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from cloudmesh.shell.command import command
 from cloudmesh.shell.command import PluginCommand
-
+from cloudmesh.street.api import Street
 
 class StreetCommand(PluginCommand):
 
@@ -11,14 +11,13 @@ class StreetCommand(PluginCommand):
         ::
 
           Usage:
-                street -f FILE
-                street FILE
-                street list
+                street sign hallo NAME
+                street sign detect IMAGE
 
           This command does some useful things.
 
           Arguments:
-              FILE   a file name
+              NAME   name of the sign
 
           Options:
               -f      specify the file
@@ -26,5 +25,17 @@ class StreetCommand(PluginCommand):
         """
         print(arguments)
 
+        if arguments.sign and arguments.hallo:
 
+            street = Street()
+            street.hello(arguments.NAME)
 
+            return ""
+
+                if arguments.sign and arguments.hallo:
+
+        elif arguments.sign and arguments.hallo:
+            
+            street = Street()
+            output = street.deploy(arguments.IMAGE)
+            print (output)
